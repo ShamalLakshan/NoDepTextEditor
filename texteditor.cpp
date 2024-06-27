c4e6d29256e60489e427eaab5dbe4f56a08ea1d2
ShamalLakshan <shamallakshan00600@gmail.com>
2024-06-27 18:42:23 +0530

    enbling-raw-mode

--- texteditor.cpp
+++ texteditor.cpp
@@ -17,3 +17,57 @@
 
 //Disabling Raw mode
 struct termios orig_termios;
+
+// ---------- Terminal ---------- //
+
+//Error Handling
+void die(const char *s) {
+    perror(s);
+    exit(1);
+}
+
+void disableRawMode() {
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+        die("tcsetattr");
+}
+
+//Turning off echo.
+//Enables Raw mode instead of the Canonical mode.
+void enableRawMode(){
+    //Error Handling
+    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+
+    //Disables raw mode at exit
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    
+    tcgetattr(STDIN_FILENO, &raw);
+
+    //IXON - Disables ctrl + s , ctrl + q...   
+    //ICRNL - Disables ctrl + m.
+    raw.c_iflag &= ~(ICRNL | IXON);
+
+    //Turning off some miscellaneous flags
+    raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+
+    //Disables output processing
+    raw.c_oflag &= ~(OPOST);
+
+    //Turning off some miscellaneous flags
+    raw.c_cflag |= (CS8);
+
+    //ICANON - reading input byte-by-byte, instead of line-by-line.
+    //IEXTEN - Disables ctrl + v.
+    //ISIG - Disables ctrl + c , ctrl + z ....
+    raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG); 
+
+    //Timeout for Read()
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
+
+    //Error Handling
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
